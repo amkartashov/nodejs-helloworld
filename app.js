127de5cb6e68ad52f1ce3c6067a1da6c9671e762
Andrey Kartashov <akartashov@odin.com>
2018-03-18 01:20:10 +0700

    now it is a webapp

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-var msg = 'Hello World';
-console.log(msg);
+var http = require('http')
+
+http.createServer(onRequest).listen(8888);
+console.log('Server has started');
+
+function onRequest(request, response){
+  response.writeHead(200);
+  response.write('Hello World');
+  response.end();
+}
